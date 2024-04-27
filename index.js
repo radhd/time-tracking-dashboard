51aa8756baac8b032a9dcc7c20d99dd9d412d7f2
Nika M <82239236+radhd@users.noreply.github.com>
2024-04-28 00:24:04 +0400

    Add files via upload
    Done

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const daily = document.getElementsByClassName('daily');
 
 const buttons = Array.from(buttonsTag);
 
-
 activeButton();
 
 // Make button active 
+
 function activeButton() {
     buttons.forEach(button => {
     
@@ -22,7 +22,7 @@ function activeButton() {
 
             });
             button.classList.add('active');
-            // showInfo(button.textContent);
+            
             showInfo(button.textContent);
         });
     });
@@ -35,6 +35,7 @@ function showInfo(buttonContentString) {
     const dailyElements = document.querySelectorAll('.daily');
     const weeklyElements = document.querySelectorAll('.weekly');
     const monthlyElements = document.querySelectorAll('.monthly');
+    
 
 
     if (buttonContentString === "Daily") {
@@ -60,4 +61,82 @@ function showInfo(buttonContentString) {
             element.style.display = displayedInfo;
         });
     };
-}
+};
+
+// FETCHING THE DATA FROM THE DATA.JSON 
+// Fetching needs an input of endpoint 
+
+
+async function fetchData(endpoint) {
+    try {
+
+        const response = await fetch(`http://localhost:3000/${endpoint}`);
+        const data = await response.json();
+
+        const title = data.title;
+
+        const dailyCurrent = data.timeframes.daily.current;
+        const dailyPrevious = data.timeframes.daily.previous;
+
+        const weeklyCurrent = data.timeframes.weekly.current;
+        const weeklyPrevious = data.timeframes.weekly.previous;
+
+        const monthlyCurrent = data.timeframes.monthly.current;
+        const monthlyPrevious = data.timeframes.monthly.previous;
+
+        return [title, dailyCurrent, dailyPrevious, weeklyCurrent, weeklyPrevious, monthlyCurrent, monthlyPrevious];
+
+    } catch(error) {
+        console.error("Error fetching data: ", error);
+        return null;
+    }
+};
+
+//create new h2
+
+const cardTitleImages = document.querySelectorAll(".card-title-image");
+
+cardTitleImages.forEach(async (cardTitleImage, index) => {
+    const title = await fetchData(index);
+
+    const newH2 = document.createElement("h2");
+    newH2.textContent = title[0];
+
+    cardTitleImage.appendChild(newH2);
+});
+
+
+// This function receives daily, weekly or monthly data, currentTimeFrame from the fetched data and previous time frame from fetched data and shows this data as a cards.
+
+function createTimeframes(dailyWeeklyMonthly, currentTimeframe,previousTimeframe) {
+    const selector = document.querySelectorAll(`.${dailyWeeklyMonthly}`);
+
+    selector.forEach(async (element, index) => {
+        const responses = await fetchData(index);
+        console.log(responses);
+        const current = responses[currentTimeframe];
+        const previous = responses[previousTimeframe];
+
+        const newSpan = document.createElement("span");
+        const newP = document.createElement("p");
+        newSpan.textContent = `${current}hrs`;
+
+        if(dailyWeeklyMonthly === "daily") {
+            newP.textContent = `Last Day - ${previous}hrs`;
+        } else if (dailyWeeklyMonthly === "weekly") {
+            newP.textContent = `Last Week - ${previous}hrs`;
+        } else {
+            newP.textContent = `Last Month - ${previous}hrs`;
+        }
+
+        newSpan.classList.add("current-hrs");
+        
+        element.appendChild(newSpan);
+        element.appendChild(newP);
+
+    });
+};
+
+createTimeframes('daily', 1, 2);
+createTimeframes('weekly', 3, 4);
+createTimeframes('monthly', 4, 5);
